bfd16e7f0efa4030561a77633c2a59766e0f5822
Magila-Ferreira <magilamorganasf@gmail.com>
2023-10-16 17:28:13 -0300

    inicializacao

--- src/main/java/edu/fatec/heranca_db/model/Aluno.java
+++ src/main/java/edu/fatec/heranca_db/model/Aluno.java
@@ -1,5 +1,30 @@
 package edu.fatec.heranca_db.model;
 
-public class Aluno {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_aluno")
+public class Aluno extends Pessoa {
+    @Id
+    @Column(length = 15,nullable = false,name = "al_ra")
+    private String ra;
     
+    @Column(length = 25,nullable = false,name = "al_curso")
+    private String curso;
+    
+    public String getRa() {
+        return ra;
+    }
+    public void serRa(String ra) {
+        this.ra = ra;
+    }
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
 }
--- src/main/java/edu/fatec/heranca_db/model/Pessoa.java
+++ src/main/java/edu/fatec/heranca_db/model/Pessoa.java
@@ -7,18 +7,57 @@ import java.io.Serializable;
 @MappedSuperclass
 public class Pessoa implements Serializable {
 
-  @Column(length = 50, nullable = false)
-  private String nome;
+	@Column(length = 50, nullable = false)
+	private String nome;
 
-  @Column(length = 12)
-  private String rg;
+	@Column(length = 12)
+	private String rg;
 
-  @Column(length = 16)
-  private String cpf;
+	@Column(length = 16)
+	private String cpf;
 
-  @Column(length = 25)
-  private String pai;
+	@Column(length = 25)
+	private String pai;
 
-  @Column(length = 25)
-  private String mae;
+	@Column(length = 25)
+	private String mae;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getPai() {
+		return pai;
+	}
+
+	public void setPai(String pai) {
+		this.pai = pai;
+	}
+
+	public String getMae() {
+		return mae;
+	}
+
+	public void setMae(String mae) {
+	}
 }
